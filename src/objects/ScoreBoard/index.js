73c764a72a357e2d8220fd6122983dab8a2ebe7a
Renato Gama <renatogama99@gmail.com>
2021-07-10 11:23:59 +0100

    :lipstick: Create ScoreBoard object

---
+++ src/objects/ScoreBoard/index.js
@@ -0,0 +1,13 @@
+import PlayerName from "../../components/PlayerName";
+
+function ScoreBoard(){
+
+    return `
+        <header class="score-board">
+            ${PlayerName("Player 1")}
+            ${PlayerName("Player 2")}
+        </header>
+    `;
+}
+
+export default ScoreBoard;
